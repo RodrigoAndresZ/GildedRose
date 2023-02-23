7a36c999152691120bbdbc738cc28ea10e22ec17
Rodrigo <rodras000@gmail.com>
2023-02-23 20:26:24 +0000

    Refactorizamos objetos para cada item

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -22,37 +22,38 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            switch ($item->name){
-                case self::BRIE:
-                {
-                    if($item->quality < 50) $item->quality++;
-                    if($item->quality < 50 and $item->sellIn < 0) $item->quality++;
-                    break;
-                }
-                case self::BACKSTAGE:
-                {
-                    if($item->sellIn < 1) $item->quality = 0;
-                    else {
-                        if ($item->quality < 50) $item->quality++;
-                        if ($item->sellIn < 11 and $item->quality < 50) $item->quality++;
-                        if ($item->sellIn < 6 and $item->quality < 50) $item->quality++;
-                    }
-                    break;
-                }
-                case self::SULFURAS:
-                {
-                    break;
-                }
-                case self::CONJURAS:{
-                    if($item->quality > 0) $item->quality -= 2;
-                    if($item->sellIn < 0 and $item->quality > 0) $item->quality -=2;
-                    break;
-                }
-                default:{
-                    if($item->quality > 0) $item->quality--;
-                    if($item->sellIn < 0 and $item->quality > 0) $item->quality--;
+            if($this->isBrie($item)){
+                if($item->quality < 50) $item->quality++;
+                if($item->quality < 50 and $item->sellIn < 0) $item->quality++;
+            }
+            if($this->isBackstage($item)){
+                if($item->sellIn < 1) $item->quality = 0;
+                else {
+                    if ($item->quality < 50) $item->quality++;
+                    if ($item->sellIn < 11 and $item->quality < 50) $item->quality++;
+                    if ($item->sellIn < 6 and $item->quality < 50) $item->quality++;
                 }
             }
+            if($this->isConjuras($item)){
+                if($item->quality > 0) $item->quality -= 2;
+                if($item->sellIn < 0 and $item->quality > 0) $item->quality -=2;
+            }
+            if(!$this->isBrie($item) and !$this->isBackstage($item) and !$this->isConjuras($item) and !$this->isSulfuras($item)){
+                if($item->quality > 0) $item->quality--;
+                if($item->sellIn < 0 and $item->quality > 0) $item->quality--;
+            }
         }
+    }
+    private function isBrie(Item $item): bool{
+        return $item->name == self::BRIE;
+    }
+    private function isBackstage(Item $item): bool{
+        return $item->name == self::BACKSTAGE;
+    }
+    private function isSulfuras(Item $item): bool{
+        return $item->name == self::SULFURAS;
+    }
+    private function isConjuras(Item $item): bool{
+        return $item->name == self::CONJURAS;
     }
 }
